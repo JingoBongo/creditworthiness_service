02f8d3485608f4184045afd1a5e6c453da60531d
JingoBongo <https://github.com/JingoBongo>
2022-03-31 19:16:04 +0300

    now it is done for good

--- simplest_microservice/simplest_endpoint.py
+++ simplest_microservice/simplest_endpoint.py
@@ -12,6 +12,10 @@ swagger = Swagger(app)
 @app.route('/hello_world')
 def hello_world():
     """Everyone starts here. So did we.
+    ---
+    responses:
+      200:
+        description: start point
     """
     return 'Hello, World!'
 
@@ -19,6 +23,10 @@ def hello_world():
 @app.route('/')
 def hello():
     """Root, please go somewhere else
+    ---
+    responses:
+      200:
+        description: why would you go here, go away
     """
     return 'try swagger at least, (apidocs)'
 
@@ -26,11 +34,9 @@ def hello():
 @app.route("/user/<string:job_id>")
 def main(job_id):
     """I have no idea why is this a title
-        These are just notes as an example. And as a note, I don't care about
-        most of it's functionality, it will be enough just putting a name&comment above each endpoint
-        just for BRIEF explanation. We are not getting paid here after all.
-        But I didn't find time to understand how to make proper examples and etc here. I don't care,
-        we can just delete everything below next line.
+        These are just notes as an example. We don't need most of this
+        functionality, plus we aren't paid for this. So let's keep it
+        simple as in hello_world endpoit, ey?
         ---
         parameters:
           - arg1: whatever, dude, this goes into business logic for now
@@ -43,17 +49,5 @@ def main(job_id):
         responses:
           200:
             description: A simple business logic unit with swagger
-            schema:
-              $ref: '{job_id}'
-            examples:
-              job_id: {"job_id": 'botan'}
         """
     return do_the_thing_function(job_id)
-
-
-# @app.route("/spec")
-# def spec():
-#     swag = swagger(app)
-#     swag['info']['version'] = "1.0"
-#     swag['info']['title'] = "Simplest API"
-#     return jsonify(swag)

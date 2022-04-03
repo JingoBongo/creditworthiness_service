d73b83e0f258c0bfb1d089d9b39e2c851a377ec1
JingoBongo <mpakaboy@gmail.com>
2022-04-03 21:25:11 +0300

    testing push trigger

--- simplest_microservice/py_runnable_simplest_endpoint.py
+++ simplest_microservice/py_runnable_simplest_endpoint.py
@@ -26,7 +26,7 @@ def hello():
       200:
         description: why would you go here, go away
     """
-    return 'try swagger at least, (apidocs)'
+    return 'try swagger at least, (apidocs). a couple new words to check that triggers work'
 
 
 @app.route("/user/<string:job_id>")

f0cbce6bfa60ddac717a1837605555d9cccfaa6a
JingoBongo <mpakaboy@gmail.com>
2022-06-19 00:04:04 +0300

    added setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='fuse_framework',
+    version='1',
+    packages=['utils', 'utils.decorators', 'blogic', 'slogic', 'endpoints', 'endpoints.system_endpoints', 'schedulers',
+              'schedulers.system_schedulers'],
+    url='https://github.com/JingoBongo/fuse_framework',
+    license='=/=',
+    author='mpaka',
+    author_email='mpakaboy@gmail.com',
+    description='=/='
+)

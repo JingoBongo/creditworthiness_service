ac9d58f39f5771d6f0d5d87b4d791395cfe16f45
JingoBongo <mpakaboy@gmail.com>
2023-01-01 14:42:36 +0200

    terminal fix1

--- endpoints/test_terminal.py
+++ endpoints/test_terminal.py
@@ -12,6 +12,8 @@ import fcntl
 import shlex
 import logging
 import sys
+
+from utils import yaml_utils
 from utils.flask_child import FuseNode
 from argparse import ArgumentParser
 
@@ -103,51 +105,12 @@ def connect():
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description=(
-            "A fully functional terminal in your browser. "
-            "https://github.com/cs01/pyxterm.js"
-        ),
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-    )
-    parser.add_argument(
-        "-p", "--port", default=5000, help="port to run server on", type=int
-    )
-    parser.add_argument(
-        "--host",
-        default="127.0.0.1",
-        help="host to run server on (use 0.0.0.0 to allow access from other hosts)",
-    )
-    parser.add_argument("--debug", action="store_true", help="debug the server")
-    parser.add_argument("--version", action="store_true", help="print version and exit")
-    parser.add_argument(
-        "--command", default="bash", help="Command to run in the terminal"
-    )
-    parser.add_argument(
-        "--cmd-args",
-        default="",
-        help="arguments to pass to command (i.e. --cmd-args='arg1 arg2 --flag')",
-    )
+    parser.add_argument('-port')
+    parser.add_argument('-local')
     args = parser.parse_args()
-    if args.version:
-        print(__version__)
-        exit(0)
-    app.config["cmd"] = [args.command] + shlex.split(args.cmd_args)
-    green = "\033[92m"
-    end = "\033[0m"
-    log_format = (
-        green
-        + "pyxtermjs > "
-        + end
-        + "%(levelname)s (%(funcName)s:%(lineno)s) %(message)s"
-    )
-    logging.basicConfig(
-        format=log_format,
-        stream=sys.stdout,
-        level=logging.DEBUG if args.debug else logging.INFO,
-    )
-    logging.info(f"serving on http://{args.host}:{args.port}")
-    socketio.run(app, debug=args.debug, port=args.port, host=args.host)
+    host = "127.0.0.1" if args.local == 'True' else yaml_utils.get_host_from_config()
+    debug = yaml_utils.get_debug_flag_from_config()
+    socketio.run(app, debug=debug, port=args.port, host=host)
 
 
 if __name__ == "__main__":

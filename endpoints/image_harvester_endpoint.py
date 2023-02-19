a5a1b1b5daa091fbf7f18438aa3366a38cbe45d4
JingoBongo <mpakaboy@gmail.com>
2023-02-19 13:20:47 +0200

    temp img harvester

--- endpoints/image_harvester_endpoint.py
+++ endpoints/image_harvester_endpoint.py
@@ -47,3 +47,30 @@ def endpoint_with_var(str_variable):
 
 if __name__ == "__main__":
     app.run()
+
+
+
+
+# import concurrent.futures
+# import time
+#
+# def execute_task(role, task, args):
+#     print(f'Actor with role "{role}" is executing task "{task}" with args "{args}"')
+#     time.sleep(2)
+#     print(f'Actor with role "{role}" finished executing task "{task}" with args "{args}"')
+#
+# def run_actors(role, task, args):
+#     with concurrent.futures.ThreadPoolExecutor() as executor:
+#         future_to_actor = {executor.submit(execute_task, role, task, args): role for role, task, args in zip(roles, tasks, args_list)}
+#         for future in concurrent.futures.as_completed(future_to_actor):
+#             actor = future_to_actor[future]
+#             try:
+#                 future.result()
+#             except Exception as e:
+#                 print(f'Actor with role "{actor}" generated an exception: {e}')
+#
+# roles = ['actor_1', 'actor_2', 'actor_3']
+# tasks = ['task_1', 'task_2', 'task_3']
+# args_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+#
+# run_actors(roles, tasks, args_list)

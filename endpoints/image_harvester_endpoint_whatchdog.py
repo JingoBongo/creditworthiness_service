a44dfc0c8e7aaa06e51836d8086919147a666f2a
JingoBongo <mpakaboy@gmail.com>
2023-03-23 16:13:11 +0200

    This is Hell. And I invite you to it
    This doesn't work on windows. on module level somehow.
    
    But for linux?
    
    does this fix memory issue? <- yes. dont save frames in memory
    added requirements.txt dynamic search & install

--- endpoints/image_harvester_endpoint_whatchdog.py
+++ endpoints/image_harvester_endpoint_whatchdog.py
@@ -243,7 +243,7 @@ def process_existing_screenshots():
     # batch = []
 
     # Collect file paths
-    theshold_of_screenshots_to_panic = 20_000
+    theshold_of_screenshots_to_panic = 10_000
 
     for filename in os.listdir(folder_path):
         file_path = os.path.join(folder_path, filename)

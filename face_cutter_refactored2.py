86fb4cd20f09141ac92179c4ca142324ac196569
JingoBongo <mpakaboy@gmail.com>
2023-04-10 22:02:42 +0300

    added old style prints 3

--- face_cutter_refactored2.py
+++ face_cutter_refactored2.py
@@ -82,7 +82,7 @@ def get_files_from_zip(zip_file_path):
 def fixBadZipfile(zipFile):
  f = open(zipFile, 'r+b')
  data = f.read()
- pos = data.find('\x50\x4b\x05\x06') # End of central directory signature
+ pos = data.find(b'\x50\x4b\x05\x06') # End of central directory signature
  if (pos > 0):
      print("Trancating file at location " + str(pos + 22)+ ".")
      f.seek(pos + 22)   # size of 'ZIP end of central directory record'

5e1e47d27ec4a99a4553b058520c2b10ff47ad8f
JingoBongo <mpakaboy@gmail.com>
2023-04-10 20:39:34 +0300

    added old style prints 3

--- face_cutter_refactored2.py
+++ face_cutter_refactored2.py
@@ -83,43 +83,43 @@ def crop_faces_in_zip(input_zip_path, output_zip_path):
         output_zip_path (str): The path to the output zip file.
     """
     print(f"1 {os.path.normpath(input_zip_path)=}")
-    input_zip = zipfile.ZipFile(os.path.normpath(input_zip_path) , 'r')
-    print(f"2 {os.path.normpath(output_zip_path)=}")
-    output_zip = zipfile.ZipFile(os.path.normpath(output_zip_path) , 'w')
-    print("3")
-    for name in input_zip.namelist():
-        print("4")
-        input_file = input_zip.open(name)
-        print("5")
-        np_img = np.frombuffer(input_file.read(), np.uint8)
-        print("6")
-        img = cv2.imdecode(np_img, cv2.IMREAD_COLOR)
-        print("7")
-        if img.shape[2] == 1:
-            print("8")
-            img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-            print("9")
-        elif img.shape[2] == 3 and img.ndim == 2:
-            print("10")
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            print("11")
-        print("12")
-        faces = face_cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=10, minSize=(30, 30))
-        print("13")
-        i = 0
-        for (x, y, w, h) in faces:
-            print("14")
-            # Crop the face region from the image
-            face = img[y:y + h, x:x + w]
-            face_img = np.array(face)
-            print("15")
-            success, buffer = cv2.imencode('.jpg', face_img)
-            print("16")
-            parts = os.path.basename(name).split('.')
-            print("17")
-            output_zip.writestr(f"{'.'.join(parts[:-1])}_{i}.jpg", buffer)
-            print("18")
-            i += 1
+    with zipfile.ZipFile(os.path.normpath(input_zip_path) , 'r') as input_zip :
+        print(f"2 {os.path.normpath(output_zip_path)=}")
+        with zipfile.ZipFile(os.path.normpath(output_zip_path) , 'w') as output_zip :
+            print("3")
+            for name in input_zip.namelist():
+                print("4")
+                input_file = input_zip.open(name)
+                print("5")
+                np_img = np.frombuffer(input_file.read(), np.uint8)
+                print("6")
+                img = cv2.imdecode(np_img, cv2.IMREAD_COLOR)
+                print("7")
+                if img.shape[2] == 1:
+                    print("8")
+                    img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+                    print("9")
+                elif img.shape[2] == 3 and img.ndim == 2:
+                    print("10")
+                    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                    print("11")
+                print("12")
+                faces = face_cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=10, minSize=(30, 30))
+                print("13")
+                i = 0
+                for (x, y, w, h) in faces:
+                    print("14")
+                    # Crop the face region from the image
+                    face = img[y:y + h, x:x + w]
+                    face_img = np.array(face)
+                    print("15")
+                    success, buffer = cv2.imencode('.jpg', face_img)
+                    print("16")
+                    parts = os.path.basename(name).split('.')
+                    print("17")
+                    output_zip.writestr(f"{'.'.join(parts[:-1])}_{i}.jpg", buffer)
+                    print("18")
+                    i += 1
 
 
 def process_one_zip(zip_path):
